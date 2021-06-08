31af9079d4ddf803a8d12e8e31238bf2a30fcec0
Sandra Bino <42898716+sandrabeme@users.noreply.github.com>
2021-06-09 01:01:05 +0530

    Update cow.py

--- cow.py
+++ cow.py
@@ -12,6 +12,8 @@ min_age_limit = 18  # Optional. By default returns centers without filtering by 
 
 cowin = CoWinAPI()
 available_centers = cowin.get_availability_by_district(district_id, date, min_age_limit)
-print(available_centers[centers])
-#for i in available_centers:
- #   print(i)
+#print(available_centers['centers'])
+for i in available_centers['centers']:
+    print(' Hospital: ' + i.get('name') + ' pincode: ' + str(i.get('pincode')) + '\n\t Minimmum age limit = ' + str(i.get('sessions')[0].get('min_age_limit')) + '\n\t vaccine name: '+ str(i.get(
+'sessions')[0].get('vaccine')) + '\n\t Dose1 left: ' + str(i.get('sessions')[0].get('available_capacity_dose1')) + '\n\t Dose2 left: ' + str(i.get('sessions')[0].get('available_capacity_dose2'))
+)
